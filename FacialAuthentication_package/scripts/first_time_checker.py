e39ba50cf10e7b945767d38e0943801f8ee5b060
Roberto Franchini <roberto.franchini@student.h-farm.com>
2020-12-14 13:48:06 +0100

    Update first_time_checker.py

--- FacialAuthentication_package/scripts/first_time_checker.py
+++ FacialAuthentication_package/scripts/first_time_checker.py
@@ -13,3 +13,22 @@ def write_used():
     with open(package_path + 'data/times.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(list_to_write)
+
+def first_time():
+    """Read the file containing the action possible,
+    
+    :return: True if never used before, otherwise False
+    :rtype: boolean
+    """
+    csv_dict = {}
+    with open(package_path + 'data/times.csv', 'r',
+              encoding='utf-8-sig') as file:
+        reader = csv.reader(file)
+        for col in reader:
+            # assign from the csv the value
+            # of the user status
+            csv_dict[col[0]] = col[1]
+        # check if the user has not used the pckg
+        if csv_dict["user"] == "0":
+            return True
+        return False
