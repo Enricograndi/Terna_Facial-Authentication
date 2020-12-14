0c32580d83f309d4e1e89b6d46cadbaa6566b662
SpessatoTommaso <74065849+SpessatoTommaso@users.noreply.github.com>
2020-12-14 19:10:55 +0100

    Update test_main.py

--- FacialAuthentication_package/Tests/test_main.py
+++ FacialAuthentication_package/Tests/test_main.py
@@ -9,14 +9,19 @@ import dbmanager as db
 class TestInput(unittest.TestCase):
     def setUp(self):
         #sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname((os.path.abspath(__file__))))))
-        db.open_or_create_db("../../facialauthentication_package/data/database.db")
+        db.open_or_create_db("facialauthentication_package/data/database.db")
 
      # smoke test: valid inputs
     def test_correct_user(self):
         # we select some valid inputs, for which the output is known
         self.assertEqual(db.check_password("totti","totti"), True)
 
- 
+    # invalid inputs
+    def test_wrong_user(self):
+        # imput of a wrong user
+        self.assertEqual(db.check_password("Totto","totti"), False)
+
+
 if __name__ == '__main__':
     unittest.main()
     
